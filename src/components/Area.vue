6f07c24c2d00995c287ec1f980d3bd7a438336df
Justus Kraft <hallo@justuskraft.de>
2026-05-27 18:15:30 +0200

    fix: move bubble styles to index.css

--- src/components/Area.vue
+++ src/components/Area.vue
@@ -226,36 +226,4 @@ onMounted(() => {
 .k-loop-group-label {
   margin-bottom: var(--spacing-2);
 }
-
-[data-comment-id] {
-  position: relative;
-}
-
-[data-comment-id] .k-frame {
-  background: none;
-}
-
-[data-comment-id]::before {
-  content: "";
-  position: absolute;
-  top: 11%;
-  left: 11%;
-  width: 78%;
-  height: 78%;
-  border-radius: 100%;
-  z-index: 2;
-  background: var(--back);
-}
-
-[data-comment-id]::after {
-  content: attr(data-comment-id);
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  font-size: 0.75rem;
-  font-weight: var(--font-bold);
-  transform: translate(-50%, -50%);
-  z-index: 3;
-  color: var(--color);
-}
 </style>
